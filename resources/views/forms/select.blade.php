216aa6b3521adfdfedd957ad37cb29dc411fd87c
Jean-Philippe Murray <jp@atomescroch.us>
2021-01-02 11:33:09 -0500

    Takes an associative array so we can properly display a label

--- resources/views/forms/select.blade.php
+++ resources/views/forms/select.blade.php
@@ -1,5 +1,5 @@
 <select {!! $attributes->merge(['class' => 'mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm']) !!}>
-    @foreach($items as $item)
-    <option value={{ $item }} @if($chosen !== false && $chosen == $item) selected @endif>{{ ucfirst($item) }}</option>
+    @foreach($items as $label => $item)
+    <option value={{ $item }} @if($chosen !== false && $chosen == $item) selected @endif>{{ $label }}</option>
     @endforeach
 </select>
